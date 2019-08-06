bd63c5f3c3c3640dbee8bd6ca644c2f393bd9a07
Alexander Cowen-Rivers <mc_rivers@icloud.com>
2019-08-06 10:09:13 +0900

    Update simple_test.py

--- tests/memory/simple_test.py
+++ tests/memory/simple_test.py
@@ -4,6 +4,7 @@ from rl.hparams.utils import HParams
 import numpy as np
 import random
 import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 
 class SimpleMemoryTest(tf.test.TestCase):
